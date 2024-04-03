791ad42ad35a88de8114bf03ac98b4570b92a444
BastianEngoe <92574751+BastianEngoe@users.noreply.github.com>
2024-04-03 11:30:07 +0200

    Stuffs

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -11,10 +12,15 @@ public class GameManager : MonoBehaviour
     public float gold;
     [Range(0.0f, 1.0f)] public float happiness;
     public float timer = 600; //10 minutes
-    
+
     [Header("Production")]
     [Range(0.0f, 1.0f)] public float produce;
     public int quota;
+    public float animalProduction = 1;
+
+    [Header("Animals")] 
+    public Transform animalPen;
+    public GameObject[] animals;
 
     private void Awake()
     {
@@ -29,8 +35,43 @@ public class GameManager : MonoBehaviour
 
         if (happiness > 0.1f)
         {
-            gold += 10 * (produce * happiness) * Time.deltaTime;
+            gold += animalProduction * (produce * happiness) * Time.deltaTime;
             happiness -= produce * 0.1f * Time.deltaTime;
+        }
+    }
+
+    public void BuyAnimal(string animal)
+    {
+        if (animal == "Hen")
+        {
+            if (gold < 50) return;
+            animalProduction += 0.1f;
+            gold -= 50;
+            Instantiate(animals[0], animalPen.position, quaternion.identity);
+        }
+
+        if (animal == "Sheep")
+        {
+            if(gold < 75 ) return;
+            animalProduction += 0.2f;
+            gold -= 75;
+            Instantiate(animals[1], animalPen.position, quaternion.identity);
+        }
+
+        if (animal == "Pig")
+        {
+            if (gold < 150) return;
+            animalProduction += 0.5f;
+            gold -= 150;
+            Instantiate(animals[2], animalPen.position, quaternion.identity);
+        }
+
+        if (animal == "Cow")
+        {
+            if (gold < 300) return;
+            animalProduction += 1.25f;
+            gold -= 300;
+            Instantiate(animals[3], animalPen.position, quaternion.identity);
         }
     }
 }
